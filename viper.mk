7a86ba9ef6c8a63ae106f2bb4fda00a0ebe29e21
soekarnohatta <nayefhaidir@outlook.co.id>
2020-10-09 11:42:18 +0000

    ugglite: Viper-ify!
    Signed-off-by: soekarnohatta <nayefhaidir@outlook.co.id>

--- viper.mk
+++ viper.mk
@@ -21,7 +21,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_m.mk)
 
 # Inherit some common Lineage stuff
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product, vendor/viper/config/common_full_phone.mk)
 
 # Inherit from ugglite device
 $(call inherit-product, $(LOCAL_PATH)/device.mk)
@@ -30,7 +30,7 @@ PRODUCT_BRAND := Xiaomi
 PRODUCT_DEVICE := ugglite
 PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_MODEL := Redmi Note 5A
-PRODUCT_NAME := lineage_ugglite
+PRODUCT_NAME := viper_ugglite
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
